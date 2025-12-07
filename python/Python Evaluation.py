caa2cd3dc82923069ac9687820e4dbbde1a3fedd
Kasturi Hanjagi <kasturi.hanjagi@gmail.com>
2025-12-07 20:13:19 +0530

    Python Evaluation Solution

---
+++ python/Python Evaluation.py
@@ -0,0 +1,5 @@
+'''
+Problem statement link: https://www.hackerrank.com/challenges/python-eval/problem?isFullScreen=true
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eval(input())
